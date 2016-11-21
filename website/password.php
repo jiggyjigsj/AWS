a58c2871f7a23cf3fd826df8bd3dfe418cc0b968
Jigar Patel <jjp1023@gmail.com>
2016-11-20 22:06:04 -0600

    Added tables and database creation data

--- website/password.php
+++ website/password.php
@@ -5,22 +5,23 @@ $password="Pat3l120133";
 
 require '/var/www/html/vendor/autoload.php';
 
-
 $client = new Aws\Rds\RdsClient([
   'region'            => 'us-west-2',
     'version'           => 'latest',
-]);
- 
+	'credentials' => [
+'key'    => 'AKIAIKYMAUHZJ7CYJEJQ',
+'secret' => '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'],
+]); 
 
 $result = $client->describeDBInstances([
     'DBInstanceIdentifier' => 'dbfirst'
 ]);
 
-
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+$endpoint=$endpoint.':3306';
+echo $endpoint;
 
-
-$conn = new mysqli($hostname,$username, $password);
+$conn = new mysqli($endpoint,$username, $password);
 
 $sql = "CREATE DATABASE IF NOT EXISTS  app";
 if ($conn->query($sql) === TRUE) {
@@ -50,13 +51,20 @@ $create_tbl = $link->query($create_table);
 $create_admin = 'CREATE TABLE IF NOT EXIST users
 (
 	id INT NOT NULL AUTO_INCREMENT.
-	username VARCHAR(20) NOT NULL,
+	username VARCHAR(50) NOT NULL,
 	password VARCHAR(30) NOT NULL,
 	feature INT(1) NULL
 )';
 $check_users = "SELECT * FROM users";
 $chk_usr = $link->query($check_users);
 
-if (mysql_num_rows($chk_usr)==0) { 
-	echo "empty";
+if (mysqli_num_rows($chk_usr)==0) { 
+
+$insert = 'INSERT INTO TABLE users (`id`,`username`,`password`) VALUES (1,"admin","admin"),(2,"jpatel74@hawk.iit.edu","123456"),(3,"controller","password")';
+
+$insert_usr = $link->query($insert);
+
 }
+
+$link->close();
+?>
