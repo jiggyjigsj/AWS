8a5283c7d8f105b1041196f531998b4b7f6e40bd
Jigar Patel <jjp1023@gmail.com>
2016-12-01 04:22:10 -0600

    middle of adding crontab

--- install-app-worker.sh
+++ install-app-worker.sh
@@ -1,13 +1,10 @@
 #! /usr/bin/env bash
 
 sudo apt-get update
-sleep 10
+
 sudo apt-get -y install git apache2 php-xml libapache2-mod-php php php-mysql curl php-curl zip unzip mysql-client php-gd
 
 cd /home/ubuntu
-curl -sS https://getcomposer.org/installer | php
-sleep 10
-php composer.phar require aws/aws-sdk-php
 
 sudo systemctl enable apache2
 sudo systemctl start apache2
@@ -15,10 +12,21 @@ sudo systemctl start apache2
 sudo git clone git@github.com:illinoistech-itm/jpatel74.git
 
 sudo rm -r /var/www/html/*
-sudo mkdir /var/www/html/vendor
-sudo mv vendor/* /var/www/html/vendor
 
 sudo mv /home/ubuntu/jpatel74/website/* /var/www/html
 sudo mv /var/www/html/password.php /var/www
 
-(crontab -l 2>/dev/null; echo "1 * * * * php /var/www/html/edit.php") | crontab -
+cd /var/www/html
+
+sudo curl -sS https://getcomposer.org/installer | sudo php
+sudo php composer.phar require aws/aws-sdk-php
+
+sudo touch /var/spool/cron/root
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "1 * * * * php /var/www/html/edit.php" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
